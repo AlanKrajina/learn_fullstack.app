b3a0474545336a67d5d7ed83d036c65fbc1fb4d2
AlanKrajina <alankrajina88@gmail.com>
2019-10-09 13:54:05 -0700

    added nested rails show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@ class UsersController < ApplicationController
     
     def show
       @user = User.find_by(:id => params[:id])
+      @lesson = Lesson.find_by(id: params[:id])
+      @lesson.user.id = @user.id
     end
   
 
