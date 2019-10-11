77218c10e92be263e5aaad390ae15c9124851651
AlanKrajina <alankrajina88@gmail.com>
2019-10-11 08:05:42 -0700

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
 User.create(name: 'Semi', email: 'semi@abc.com')
 
-Comment.create(text: "Great lesson!")
-Comment.create(text: "Update would be great")
+comment1 = Comment.create(text: "Great lesson!")
+comment2 = Comment.create(text: "Update would be great")
 
-Lesson.create(title: "Rails with JavaScript Portfolio Project", content: "In this assessment you are going to expand upon your previous Rails project assessment. The goal is to add dynamic features to your previous Rails application that are possible only through JavaScript and a JSON API.", user_id:1, comment_id:1)
-Lesson.create(title: "Rails Portfolio Project", content: "In this lesson you're going to build a complete Ruby on Rails application that manages related data through complex forms and RESTful routes. The goal of the application is to build a Content Management System, whether the content being managed is Blog Posts, Recipes, a Library of Resources, or any domain model that lends itself to these requirements (the majority of ideas you could come up with would probably meet the requirements).", user_id:1, comment_id:2)
+Lesson.create(title: "Rails with JavaScript Portfolio Project", content: "In this assessment you are going to expand upon your previous Rails project assessment. The goal is to add dynamic features to your previous Rails application that are possible only through JavaScript and a JSON API.", user_id:1, comment_id:comment1)
+Lesson.create(title: "Rails Portfolio Project", content: "In this lesson you're going to build a complete Ruby on Rails application that manages related data through complex forms and RESTful routes. The goal of the application is to build a Content Management System, whether the content being managed is Blog Posts, Recipes, a Library of Resources, or any domain model that lends itself to these requirements (the majority of ideas you could come up with would probably meet the requirements).", user_id:1, comment_id:comment2)
 
+rating1 = Rating.create(stars: "9", lesson_id: 1)
+rating2 = Rating.create(stars: "10", lesson_id: 1)
+rating3 = Rating.create(stars: "5", lesson_id: 2)
