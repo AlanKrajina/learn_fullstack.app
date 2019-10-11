416cfecbba6f6e0c666e544c5f1f02e763ba9598
AlanKrajina <alankrajina88@gmail.com>
2019-10-11 07:50:25 -0700

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(name: 'Semi', email: 'semi@abc.com')
+
+Comment.create(text: "Great lesson!")
+Comment.create(text: "Update would be great")
+
+Lesson.create(title: "Rails with JavaScript Portfolio Project", content: "In this assessment you are going to expand upon your previous Rails project assessment. The goal is to add dynamic features to your previous Rails application that are possible only through JavaScript and a JSON API.", user_id:1, comment_id:1)
+Lesson.create(title: "Rails Portfolio Project", content: "In this lesson you're going to build a complete Ruby on Rails application that manages related data through complex forms and RESTful routes. The goal of the application is to build a Content Management System, whether the content being managed is Blog Posts, Recipes, a Library of Resources, or any domain model that lends itself to these requirements (the majority of ideas you could come up with would probably meet the requirements).", user_id:1, comment_id:2)
+
